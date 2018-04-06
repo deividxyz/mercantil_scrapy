db6a271d4309e7ff2f7beccb54652eabc6750ffc
David Inostroza <deivid@deivid.xyz>
2018-04-06 19:20:42 -0300

    Delay para evitar timeout

--- mercantil/start.sh
+++ mercantil/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo Iniciando tor...
 service tor start
+sleep 5
+echo Iniciando privoxy...
 service privoxy start
+sleep 5
+echo Calentando motores...
+sleep 3
 scrapy crawl mercantil36 -o mercantil.csv
